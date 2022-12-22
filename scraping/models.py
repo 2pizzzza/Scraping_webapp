ddb687f8afeea4b2083c745263be23eb1f01b27a
2pizzzza <amit.kurbanov.123.com@gmail.com>
2022-12-22 23:35:22 +0600

    test2

--- scraping/models.py
+++ scraping/models.py
@@ -2,11 +2,24 @@ from django.db import models
 
 class City(models.Model):
     name = models.CharField(max_length=50,
-                            verbose_name='Название населенного пункта')
-    slug = models.CharField(max_length=50,blank=True)
+                            verbose_name='Название населенного пункта',
+                            unique=True)
+    slug = models.CharField(max_length=50,blank=True, unique=True)
 
     class Meta:
         verbose_name='Название населенного пункта'
         verbose_name_plural='Название населенных пунктов'
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Язык программирование',
+                            unique=True)
+    slug = models.CharField(max_length=50,blank=True, unique=True)
+
+    class Meta:
+        verbose_name='Язык программирование'
+        verbose_name_plural='Языки программирования'
     def __str__(self):
         return self.name
