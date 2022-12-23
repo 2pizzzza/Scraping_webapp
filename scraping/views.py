a527c24db411d11a4404c6a541b5c0cc9acba123
2pizzzza <amit.kurbanov.123.com@gmail.com>
2022-12-23 20:37:52 +0600

    test8

--- scraping/views.py
+++ scraping/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 
+from .forms import FindForm
 from .models import City, Language, Vacancy
 def home_view(request):
-    qs = Vacancy.objects.all()
+    # print(request.GET)
+    form = FindForm
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__slug'] = city
+        if language:
+            _filter['language__slug'] = language
+
+        qs = Vacancy.objects.filter(**_filter)
+        qs = qs[::-1]
     context = {
         'object_list': qs,
+        'form': form,
     }
     return render(request, 'scraping/home.html', context)
